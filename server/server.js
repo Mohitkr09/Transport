18ba29eadd305273c95660df55a1a13224ba27fd
Mohitkr09 <mkt27858@gmail.com>
2026-05-08 10:08:11 +0530

    server fix

--- server/server.js
+++ server/server.js
@@ -570,3 +570,17 @@ socket.on(
     );
   }
 });
+
+/* =====================================================
+START SERVER
+===================================================== */
+
+const PORT =
+  process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+
+  console.log(
+    `🚀 Server running on port ${PORT}`
+  );
+});
